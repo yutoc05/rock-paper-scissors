0a6022ee9cbb91900ab16467e0ea392bf171c605
Yuto Chau <yuto5@outlook.com>
2026-03-23 21:21:57 -0400

    implement game logic to play a single round

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   let choice = Math.random();
   if (choice < 1 / 3) {
@@ -12,3 +15,33 @@ function getComputerChoice() {
 function getHumanChoice() {
   return prompt("Let's play rock paper scissors! Which do you choose?");
 }
+
+function capitalize(str) {
+  return str[0].toUpperCase() + str.slice(1).toLowerCase();
+}
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase();
+  if (humanChoice === computerChoice) {
+    console.log("You tied!");
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log(
+      `You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`,
+    );
+    humanScore++;
+  } else {
+    console.log(
+      `You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`,
+    );
+    computerScore++;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
