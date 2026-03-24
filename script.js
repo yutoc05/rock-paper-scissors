f3d8d0443f9d6bd5f59d7898e41e7b7221b82854
Yuto Chau <yuto5@outlook.com>
2026-03-23 21:53:03 -0400

    add playGame function to encapsulate scoring logic for 5 rounds

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 function getComputerChoice() {
   let choice = Math.random();
   if (choice < 1 / 3) {
@@ -20,28 +17,50 @@ function capitalize(str) {
   return str[0].toUpperCase() + str.slice(1).toLowerCase();
 }
 
-function playRound(humanChoice, computerChoice) {
-  humanChoice = humanChoice.toLowerCase();
-  if (humanChoice === computerChoice) {
-    console.log("You tied!");
-  } else if (
-    (humanChoice === "rock" && computerChoice === "scissors") ||
-    (humanChoice === "paper" && computerChoice === "rock") ||
-    (humanChoice === "scissors" && computerChoice === "paper")
-  ) {
-    console.log(
-      `You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`,
-    );
-    humanScore++;
-  } else {
-    console.log(
-      `You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`,
-    );
-    computerScore++;
+function playGame() {
+  let humanScore = 0;
+  let computerScore = 0;
+
+  function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === computerChoice) {
+      console.log("You tied!");
+    } else if (
+      (humanChoice === "rock" && computerChoice === "scissors") ||
+      (humanChoice === "paper" && computerChoice === "rock") ||
+      (humanChoice === "scissors" && computerChoice === "paper")
+    ) {
+      console.log(
+        `You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`,
+      );
+      humanScore++;
+    } else {
+      console.log(
+        `You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`,
+      );
+      computerScore++;
+    }
   }
+
+  for (let i = 0; i < 5; i++) {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+
+    playRound(humanSelection, computerSelection);
+  }
+
+  let result;
+  if (humanScore === computerScore) {
+    result = "tied";
+  } else if (humanScore > computerScore) {
+    result = "win";
+  } else {
+    result = "lose";
+  }
+
+  console.log(
+    `The game is over. You ${result}! Final score: ${humanScore}-${computerScore}`,
+  );
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-playRound(humanSelection, computerSelection);
+playGame();
