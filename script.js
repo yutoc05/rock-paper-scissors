bd77163e5e112d7c130cab1f6519116e6b06648d
Yuto Chau <yuto5@outlook.com>
2026-04-05 19:52:39 -0400

    add buttons for player choices and update playRound to display results on the page

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-  let choice = Math.random();
+  const choice = Math.random();
   if (choice < 1 / 3) {
     return "rock";
   } else if (choice < 2 / 3) {
@@ -23,30 +23,29 @@ function playGame() {
 
   function playRound(humanChoice, computerChoice) {
     humanChoice = humanChoice.toLowerCase();
+    const results = document.createElement("div");
     if (humanChoice === computerChoice) {
-      console.log("You tied!");
+      results.textContent = "You tied!";
     } else if (
       (humanChoice === "rock" && computerChoice === "scissors") ||
       (humanChoice === "paper" && computerChoice === "rock") ||
       (humanChoice === "scissors" && computerChoice === "paper")
     ) {
-      console.log(
-        `You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}`,
-      );
+      results.textContent = `You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}.`;
       humanScore++;
     } else {
-      console.log(
-        `You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}`,
-      );
+      results.textContent = `You lose! ${capitalize(computerChoice)} beats ${capitalize(humanChoice)}.`;
       computerScore++;
     }
+    results.textContent += ` Current score: ${humanScore}-${computerScore}`;
+    document.body.append(results);
   }
 
-  for (let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-
-    playRound(humanSelection, computerSelection);
+  const buttons = document.querySelectorAll("button");
+  for (const button of buttons) {
+    button.addEventListener("click", () => {
+      playRound(button.className, getComputerChoice());
+    });
   }
 
   let result;
